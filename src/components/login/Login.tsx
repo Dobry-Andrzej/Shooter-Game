04b8a983ea2aafc82bad2dfe62443c36394d5c35
Majk <michal.larysz91@gmail.com>
2020-03-13 06:44:09 +0100

    Login form frontend validation
    Wstępna walidacja

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,22 +1,51 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Login.css';
-class Login extends Component {
+import { useForm } from 'react-hook-form';
 
-    render() {
+
+const Login = () => {
+
+        const { register, handleSubmit,  errors } = useForm();
+
+        const onSubmit = (data:any) => {
+            console.warn(data);
+        };
+
         return (
-            <div className="menuContainer">
-                <div className="titleGame">Witaj w naszej kochanej grze przeglądarkowej</div>
-                <div className="loginFormContainer">
-                    <form className="loginForm">
-                        <input type="text" placeholder="Nickname"/>
-                        <input type="password" placeholder="Password"/>
-                        <button>Create</button>
-                        <p className="message"> Masz juz konto <p>Sign In</p></p>
-                    </form>
+            <div className="Login">
+                <div className="menuContainer">
+                    <div className="titleGame">Let's Shoot Someone!</div>
+                    <div className="loginFormContainer">
+                        <form onSubmit={handleSubmit(onSubmit)} className="loginForm">
+                            <div>
+                                <input type="text"
+                                       placeholder="Nickname"
+                                       name = "login"
+                                       ref={register(
+                                           { required: true,
+                                               maxLength: 20 }
+                                       )}
+                                />
+                            </div>
+                            {errors.login && <p className="login-error">Login is required!</p>}
+                            <div>
+                                <input type="password"
+                                       placeholder="Password"
+                                       name = "password"
+                                       ref={register(
+                                           { required: true, maxLength: 20,
+                                               minLength: 6 }
+                                       )}
+                                />
+                            </div>
+                            {errors.password && <p className="login-error">Password is required and must be longer than 6 characters!</p>}
+                            <button type="submit">Sign In</button>
+                            <p className="message"> You don't have an account? <button>Register</button></p>
+                        </form>
+                    </div>
                 </div>
             </div>
-        )
-    }
+        );
 }
 export default Login;
 
