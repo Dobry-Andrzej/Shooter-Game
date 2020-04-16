efa474aeab72c8d20323ab4062151a7236c0be12
Majk <michal.larysz91@gmail.com>
2020-04-16 22:45:19 +0200

    Walidacja formularza rejestracji

--- src/components/register/Register.tsx
+++ src/components/register/Register.tsx
@@ -7,8 +7,16 @@ import history from "../../routing/History";
 interface AppState{
     username: any,
     password: any,
-    email: any
+    email: any,
+    usernameError: any,
+    passwordError: any,
+    emailError: any,
+    errors: []
 }
+
+const validEmailRegex =
+    RegExp(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i);
+
 
 class Register extends Component<{}, AppState> {
 
@@ -19,6 +27,10 @@ class Register extends Component<{}, AppState> {
             username: "",
             password: "",
             email: "",
+            usernameError: "",
+            passwordError: "",
+            emailError: "",
+            errors: []
         };
 
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -27,6 +39,30 @@ class Register extends Component<{}, AppState> {
 
     handleChange =(event: React.ChangeEvent<HTMLInputElement>) => {
         this.setState({ ...this.state, [event.currentTarget.name]: String(event.target.value) });
+        const {name , value} = event.target;
+        let uError, pError, eError;
+        switch(name) {
+            case 'username':
+                uError = value.length < 5
+                    ? 'Username must be 5 characters long!'
+                    : 'OK';
+                this.setState({usernameError: uError});
+                break;
+            case 'password':
+                pError = value.length < 8
+                    ? 'Password must be 8 characters long!'
+                    : 'OK';
+                this.setState({passwordError: pError});
+                break;
+            case 'email':
+                eError = validEmailRegex.test(value)
+                    ? 'OK'
+                    : 'Email is not valid!';
+                this.setState({emailError: eError});
+                break;
+            default:
+                break;
+        }
     };
 
     handleSubmit(event:any) {
@@ -37,6 +73,7 @@ class Register extends Component<{}, AppState> {
             password: password,
             email: email
         }).then((ret) => {
+            console.warn(ret);
             if (ret.status == 200) {
                 history.push({
                     pathname: '/',
@@ -46,6 +83,7 @@ class Register extends Component<{}, AppState> {
                 });
             }
         }, (error) => {
+            console.warn(error);
             if (error.response.status == 401) {
                 console.log('UNATHORIZED USER');
             }
@@ -55,6 +93,11 @@ class Register extends Component<{}, AppState> {
 
 
     render() {
+
+        const { usernameError, passwordError, emailError } = this.state;
+        const enabled =
+            usernameError == 'OK' && passwordError == 'OK' && emailError == 'OK';
+
         return (
             <div className="menuContainer">
                 <div className="titleGame">Register to unlock ranked games!</div>
@@ -66,19 +109,22 @@ class Register extends Component<{}, AppState> {
                                value={this.state.username}
                                onChange={this.handleChange}
                         />
+                        <p className="register-error">{ this.state.usernameError }</p>
                         <input type="password"
                                placeholder="Password"
                                name="password"
                                value={this.state.password}
                                onChange={this.handleChange}
                         />
+                        <p className="register-error">{ this.state.passwordError }</p>
                         <input type="text"
                                placeholder="Email"
                                name="email"
                                value={this.state.email}
                                onChange={this.handleChange}
                         />
-                        <Button variant="contained" color="primary" type="submit">Create account</Button>
+                        <p className="register-error">{ this.state.emailError }</p>
+                        <Button disabled={!enabled} variant="contained" color="primary" type="submit">Create account</Button>
                         <p className="message"> Do you have an account?
                             <Button variant="contained" color="primary" size="small" onClick={() => history.push("/Login")}>Sign In</Button>
                         </p>
